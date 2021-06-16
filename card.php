c16b783124c1d674e82b9d82ec33e538744078d9
joy-10 <joydeepdey648@gmail.com>
2021-06-16 09:12:29 +0530

    initial

---
+++ card.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  <div class="row">
+    <div class="mb-3 col-md-4 col-lg-3 col-sm-12">
+      <div style="width: 100%;" class='card h-100'>  
+      <div class='card-body d-flex flex-column' bg='light'>
+        <h5 class='card-title mt-auto'>heading</h5>
+        <p class='card-text mt-auto'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque velit alias! Optio ipsa autem suscipit architecto consectetur, laboriosam libero ut ex fugiat sapiente impedit quibusdam debitis quo provident ea.
+        </p>
+        <a href="#" style="margin-bottom: 3%;" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    </div>
+  
+    <div class="mb-3 col-md-4 col-lg-3 col-sm-12">
+      <div style="width: 100%;" class='card h-100'>  
+      <div class='card-body d-flex flex-column' bg='light'>
+        <h5 class='card-title mt-auto'>heading</h5>
+        <p class='card-text mt-auto'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque velit alias! Optio ipsa autem suscipit architecto consectetur, laboriosam libero ut ex fugiat sapiente impedit quibusdam debitis quo provident ea.
+        </p>
+        <a href="#" style="margin-bottom: 3%;" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    </div>
+  
+    <div class="mb-3 col-md-4 col-lg-3 col-sm-12">
+      <div style="width: 100%;" class='card h-100'>  
+      <div class='card-body d-flex flex-column' bg='light'>
+        <h5 class='card-title mt-auto'>heading</h5>
+        <p class='card-text mt-auto'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque velit alias! Optio ipsa autem suscipit architecto consectetur, laboriosam libero ut ex fugiat sapiente impedit quibusdam debitis quo provident ea.
+        </p>
+        <a href="#" style="margin-bottom: 3%;" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    </div>
+  
+    <div class="mb-3 col-md-4 col-lg-3 col-sm-12">
+      <div style="width: 100%;" class='card h-100'>  
+      <div class='card-body d-flex flex-column' bg='light'>
+        <h5 class='card-title mt-auto'>heading</h5>
+        <p class='card-text mt-auto'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque velit alias! Optio ipsa autem suscipit architecto consectetur, laboriosam libero ut ex fugiat sapiente impedit quibusdam debitis quo provident ea.
+        </p>
+        <a href="#" style="margin-bottom: 3%;" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    </div>
+  
+    <div class="col-md-4 col-lg-3 col-sm-12">
+      <div style="width: 100%;" class='card h-100'>  
+      <div class='card-body d-flex flex-column' bg='light'>
+        <h5 class='card-title mt-auto'>heading</h5>
+        <p class='card-text mt-auto'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque velit alias! Optio ipsa autem suscipit architecto consectetur, laboriosam libero ut ex fugiat sapiente impedit quibusdam debitis quo provident ea.
+        </p>
+        <a href="#" style="margin-bottom: 3%;" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    </div>
+  </div>
+</body>
+</html>
+  
---
+++ form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container col-lg-6 offset-lg-3 mt-4 ml-2">
+
+    <form clas='col-lg-6 offset-lg-3'>
+
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+
+        <input style="margin-bottom: 3%;"
+        class="btn btn-primary" type="submit">
+
+      
+    </form>
+  </div>
+</body>
+</html>
